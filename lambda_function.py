ea8477305745482c6a0b5dd1e95f60bb151608ce
Mahesh-Nichenakolla <158270916+Mahesh-Nichenakolla@users.noreply.github.com>
2024-06-12 15:34:40 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -15,4 +15,4 @@ def calculate_sum():
         return render_template('result.html', num1=num1, num2=num2, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
